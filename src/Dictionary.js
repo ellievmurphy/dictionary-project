68eb6cff411989ca4e874c692c0002ca4ab1d4e6
evmurphy <evmurphy@ncsu.edu>
2021-09-20 20:38:51 -0400

    Added Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
@@ -7,8 +8,11 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
+  function handleResponse(response) {}
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     event.preventDefault();
